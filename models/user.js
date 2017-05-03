28bf921ffed2edda9cf7105b4ccaf1e71a57e42d
Stephen Wright <sjw948@gmail.com>
2017-05-03 08:42:45 +0100

    built user creation, getByUsername and getByEmail API endpoints

--- models/user.js
+++ models/user.js
@@ -4,24 +4,39 @@ const config = require('../config/database')
 
 // User Schema
 const UserSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
   firstName: {
     type: String
   },
   lastName: {
     type: String
   },
-  email: {
+  password: {
     type: String,
     required: true
+  },
+  userId: {
+    type: Number
   },
   username: {
-    type: String,
-    required: true
-  },
-  password: {
     type: String,
     required: true
   }
 });
 
 const User = module.exports = mongoose.model('User', UserSchema)
+
+module.exports.getByEmail = function(userObject, callback){
+  User.find(userObject, callback)
+}
+
+module.exports.getByUsername = function(userObject, callback) {
+  User.find(userObject, callback)
+}
+
+module.exports.create = function(userObject, callback) {
+  userObject.save(callback)
+}
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,81 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
+const Counter = require('../models/counter');
+
+router.post("/create", (req, res, next) => {
+
+  // newUser object to submit
+  let newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  // define counter to be called for assigning unique userID's
+  let counter = {
+    name: "userId"
+  }
+
+  Counter.getOne(counter, (err, callback) => {
+    if(err) throw(err)
+    if(callback) {
+      newUser.userId = callback.count // assign unique id to new user
+      User.create(newUser, (err, callback) => {
+        if(err) throw(err)
+        if(callback) {
+          let newCounterValue = newUser.userId + 1
+          Counter.increment({name: "userId", count: newCounterValue}, (err, callback) => {
+            if(err) throw(err)
+            if(callback) {
+              res.json({success: true, message: "User creation successful"})
+            }
+          })
+        } else {
+          res.json({success: true, message: "User creation failed"})
+        }
+      })
+    } else {
+      res.json({success: false, message: "Failed to get counter"})
+    }
+  })
+})
+
+// get by email
+
+router.post("/getByEmail", (req, res, next) => {
+  let userObject = {
+    email: req.body.email
+  }
+
+  User.getByEmail(userObject, (err, callback) => {
+    if(err) throw(err)
+    if(callback) {
+      res.json({success: true, message: callback})
+    } else {
+      res.json({success: false, message: "Email not found"})
+    }
+  })
+})
+
+// get by username
+
+router.post("/getByUsername", (req, res, next) => {
+  let userObject = {
+    username: req.body.username
+  }
+
+  User.getByUsername(userObject, (err, callback) => {
+    if(err) throw(err)
+    if(callback) {
+      res.json({success: true, message: callback})
+    } else {
+      res.json({success: false, message: "Username not found"})
+    }
+  })
+})
 
 // register
 router.post("/register", (req, res, next) => {
@@ -15,14 +90,14 @@ router.post("/register", (req, res, next) => {
     email: req.body.email,
     password: req.body.password
   })
-  
+
   // define counter to be called for assigning unique userID's
   let counter = {
     name: "userId"
   }
 
   // assign a unique ID provided by the counter
-  Counter.getCounter(counter, (err, callback) => {
+  Counter.getOne(counter, (err, callback) => {
     if(err) throw(err)
     if(callback) {
       newUser.userId = callback.userId // assign unique id to new user
@@ -39,12 +114,12 @@ router.post("/register", (req, res, next) => {
               res.json({success: false, message: "Username already exists"})
             } else {
               // send newUser object to registerUser function (check Models), password will be hashed, user stored on MongoDB and callback returned if successful
-              User.registerUser(newUser, (err, callback) => {
+              User.create(newUser, (err, callback) => {
                 if(err) throw(err)
                 if(callback) {
                   // set the new counter value for incrementing
                   let newCounterValue = newUser.userId + 1
-                  Counter.increment({name: "userId", count: newCounterValue }, (err, callback) => {
+                  Counter.increment({name: "userId", count: newCounterValue}, (err, callback) => {
                     if(err) throw(err)
                     if(callback) {
                       res.json({success: true, message: "Registration successful"})
