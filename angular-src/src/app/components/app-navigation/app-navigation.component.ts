bcb8e50ebd1b46265aa3e9138e5f1d807a68d3c0
Stephen Wright <sjw948@gmail.com>
2017-05-04 14:04:39 +0100

    built and tested js function for submenu expansion on app-navigation

--- angular-src/src/app/components/app-navigation/app-navigation.component.ts
+++ angular-src/src/app/components/app-navigation/app-navigation.component.ts
@@ -9,7 +9,31 @@ export class AppNavigationComponent implements OnInit {
 
   constructor() { }
 
+  // set active submenu used in conjunction with ngstyle to toggle visible menu
+  activeSubMenu: number = -1;
+
   ngOnInit() {
   }
 
+  setActiveSubMenu(menuIndex) {
+    if(this.activeSubMenu == menuIndex) {
+      this.activeSubMenu = -1
+    } else {
+      this.activeSubMenu = menuIndex
+    }
+  }
+
+  subMenuStyle(menuIndex) {
+    let menus = document.getElementsByClassName("second-level-menu")
+    if(menuIndex == this.activeSubMenu) {
+      let menuHeight = (37 * menus[menuIndex].children.length) + "px"
+      return {"max-height": menuHeight}
+    } else {
+      return {"max-height": "0px"}
+    }
+  }
+
 }
+
+// set active list which is monitored by angular
+// set style for list
